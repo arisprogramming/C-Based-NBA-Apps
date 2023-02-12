e55ce37047f389148160904ab23c4e090cfc9e63
arisprogramming <73849124+arisprogramming@users.noreply.github.com>
2023-02-12 23:41:07 +0000

    Add files via upload

---
+++ NBA All Star Voting System.c
@@ -0,0 +1,49 @@
+/* 													PROJECT PRESENTATION
+														   
+														   OBJECTIVE
+									This project  is to build an automatic voting stystem 
+
+----------------------------------------------------------------------------------------------------------------------------------------------------	
+														 PROJECT SCOPE
+							      This project should be completely ended by the en of February
+		
+----------------------------------------------------------------------------------------------------------------------------------------------------
+													   PROJECT DESCRIPTION
+							- First, when the user comes he is welcomed to the voting system, with players in east
+							- Second he is asked to type where he wanted to vote, East or West
+							- Then after that, he is aked to put it the name of the player he wanted to vote 
+							- He is then asked if he wants to vote in West as well (if has choosed East first)  
+
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+														MAIN FEATURES 
+					- Ability for system manager to store players and classify them into east and west 
+					- Ability for users to vote players in west and east
+					- Only one vote possible per user
+					- User are identified by their names, their surnames, their id card, and their address
+					- Votes are counted for each players. The one with the most votes has the captain badge near his name
+					- Votes are registred into a file where it is mentionned their names and players they voted for 
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+- 
+
+
+
+*/
+
+
+#include <stdio.h>
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+	printf("WELCOME TO THE NBA ALL STAR GAME VOTING SYSTEM\n");
+	printf("-----------------------------------------------------------------------------------------------");
+
+}
+
